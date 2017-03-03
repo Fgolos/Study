a8a944b5808aa396f2688fcd447f2242447aaa60
FDR <fgolos@yandex.ru>
2017-03-03 17:48:33 +0300

    Test

--- src/main/java/test/User.java
+++ src/main/java/test/User.java
@@ -1,10 +1,34 @@
 package test;
 
+import java.io.IOException;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 /**
  * Created by FDR on 03.03.2017.
  */
 public class User {
-    public static void main(String[] args) {
 
+    OkHttpClient client = new OkHttpClient();
+
+    public static void main(String[] args) throws IOException {
+        User example = new User();
+        String response = example.run("http://ya.ru");
+        System.out.println(response);
+    }
+
+    String run(String url) throws IOException {
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+        try (Response response = client.newCall(request).execute()) {
+            return response.body().string();
+        }
     }
 }
+
+
+
+
+
