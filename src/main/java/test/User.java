0296f3b863f545361c69170bff41138afe582533
FDR <fgolos@yandex.ru>
2017-03-03 16:52:43 +0300

    Test

---
+++ src/main/java/test/User.java
@@ -0,0 +1,10 @@
+package test;
+
+/**
+ * Created by FDR on 03.03.2017.
+ */
+public class User {
+    public static void main(String[] args) {
+        System.out.println("Привет БРАТ!");
+    }
+}
