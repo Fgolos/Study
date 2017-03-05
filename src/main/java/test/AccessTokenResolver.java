48a32dbe9736e47dcf649ab4c18b2738720bfb1d
FDR <fgolos@yandex.ru>
2017-03-05 20:35:33 +0300

    Test upload photo

---
+++ src/main/java/test/AccessTokenResolver.java
@@ -0,0 +1,32 @@
+package test;
+
+import com.vk.api.sdk.client.TransportClient;
+import com.vk.api.sdk.client.VkApiClient;
+import com.vk.api.sdk.client.actors.UserActor;
+import com.vk.api.sdk.exceptions.ApiException;
+import com.vk.api.sdk.exceptions.ClientException;
+import com.vk.api.sdk.httpclient.HttpTransportClient;
+import com.vk.api.sdk.objects.UserAuthResponse;
+
+
+/**
+ * Created by FDR on 04.03.2017.
+ */
+public class AccessTokenResolver {
+    public static void main(String[] args) throws ClientException, ApiException {
+
+        TransportClient transportClient = HttpTransportClient.getInstance();
+        VkApiClient vk = new VkApiClient(transportClient);
+        int appId = 5908645;
+        String code = "7e3404d6dbce5a8d47";
+        String clientSecret = "XAwD3qvLx5wWvCzc6vNj";
+        String redirectURL = "";
+        UserAuthResponse authResponse = vk.oauth()
+                .userAuthorizationCodeFlow(appId, clientSecret, redirectURL, code)
+                .execute();
+
+        UserActor actor = new UserActor(authResponse.getUserId(), authResponse.getAccessToken());
+        System.out.println(authResponse.getAccessToken());
+        System.out.println(authResponse.getUserId());
+    }
+}
