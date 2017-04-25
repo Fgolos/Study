802036517b479a843cc036fb71f864fe25978d5f
FDR <fgolos@yandex.ru>
2017-04-25 14:51:28 +0300

    Проверка 25.04.17

---
+++ src/test/java/ClockTest.java
@@ -0,0 +1,15 @@
+import com.home.clock.Clock;
+import org.junit.Test;
+
+
+/**
+ * Created by FDR on 25.04.2017.
+ */
+public class ClockTest {
+    @Test
+    public void addSecondsWorksCorrectly() {
+        Clock clock = new Clock();
+    }
+
+
+}
