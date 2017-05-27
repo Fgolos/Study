0f58902b8f9937b525863fdf573158cca76e87b3
FDR <fgolos@yandex.ru>
2017-05-27 18:38:51 +0300

    гуй создание пользователя

--- src/main/java/com/home/newProject/Gui.java
+++ src/main/java/com/home/newProject/Gui.java
@@ -11,15 +11,22 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+import java.io.Serializable;
+
 /**
  * Created by FDR on 27.05.2017.
  */
-public class Gui extends Application {
+public class Gui extends Application implements Serializable {
+
+
     public static void main(String[] args) {
         launch(args);
+
     }
 
     public void start(Stage myStage) {
+        Serial serial = new Serial("test.out");
         myStage.setTitle("Project");
         FlowPane flowPane = new FlowPane(10, 10);
         flowPane.setAlignment(Pos.CENTER);
@@ -30,17 +37,34 @@ public class Gui extends Application {
         TextField textFieldName = new TextField("");
         Label labelSurname = new Label("Введите Фамилию");
         TextField textFieldSurname = new TextField("");
-        Button enterButton = new Button("ENTER");
+        Button uploadButton = new Button("Upload");
+        Button downloadButton = new Button("Download");
 
-        enterButton.setOnAction(new EventHandler<ActionEvent>() {
+        uploadButton.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
                 User user = new User(textFieldName.getText(), textFieldSurname.getText());
-                System.out.println(user);
+
+                try {
+                    serial.writeUserToFile(user);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }downloadButton.setOnAction(new EventHandler<ActionEvent>() {
+                    @Override
+                    public void handle(ActionEvent event) {
+                        try {
+                            System.out.println(serial.retriveFromFile());
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        } catch (ClassNotFoundException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
             }
         });
 
-        flowPane.getChildren().addAll(labelName, textFieldName, labelSurname, textFieldSurname, enterButton);
+        flowPane.getChildren().addAll(labelName, textFieldName, labelSurname, textFieldSurname, uploadButton, downloadButton);
         myStage.show();
 
 
