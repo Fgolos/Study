fa3bf452a4fd28d7db74a3cb2dd0bc8c6c6b8aea
Egor Goloshchapov <egor.goloshchapov@siemens.com>
2017-06-13 19:52:32 +0300

    test for unexist file added

--- src/test/java/com/home/newProject/UsersTestSerial.java
+++ src/test/java/com/home/newProject/UsersTestSerial.java
@@ -1,23 +1,38 @@
 package com.home.newProject;
 
-import org.junit.Test;
-
-import java.io.IOException;
-
+import com.google.common.io.Files;
 import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
 
-/**
- * Created by FDR on 09.06.2017.
- */
+
 public class UsersTestSerial {
-    @Test
-    public void test2() throws IOException, ClassNotFoundException {
-        Users users = new Users();
-        Serial serial = new Serial("test.out");
-        serial.writeUserToFile(users);
+  @Test
+  public void test2()
+  throws IOException, ClassNotFoundException {
+    Users users = new Users();
+    Serial serial = new Serial("test.out");
+    serial.writeUserToFile(users);
 
-        Users users1 = serial.retriveFromFile();
-        assertEquals(users, users1);
+    Users users1 = serial.retriveFromFile();
+    assertEquals(users, users1);
+  }
 
-    }
+  @Test
+  public void whenSerialTriesToReadFromNonExistFile_emptyListReturned_andFileCreated()
+  throws Exception {
+    //given
+    final File tempDir = Files.createTempDir();
+    final String unexistFileName = UUID.randomUUID().toString();
+    final File file = new File(tempDir, unexistFileName);
+    assertEquals(file.exists(), false);
+    //when
+    final Serial serial = new Serial(file.getAbsolutePath());
+    final Users users = serial.retriveFromFile();
+    //then
+    assertEquals(users.users.size(), 0);
+    assertEquals(file.exists(), true);
+  }
 }
