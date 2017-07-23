a0eba1e3c3fc8e7923e169b1008de957c373ef86
FDR <fgolos@yandex.ru>
2017-07-23 12:21:57 +0300

    Золотое сечение. Работает

--- src/main/java/com/home/findMinFunction/Logika.java
+++ src/main/java/com/home/findMinFunction/Logika.java
@@ -6,33 +6,26 @@ package com.home.findMinFunction;
 public class Logika {
     public static void main(String[] args) {
         Logika logika = new Logika();
-        LineSegment lineSegment = new LineSegment(-17.0, 5.0);
-        double min = 0;
-        double raznica = 0;
-        for (int i = 0; i < 1000; i++) {
-            LineSegment newLineSegment = logika.createNewLineSegment(logika.findFirstPoint(lineSegment), logika.findSecondPoint(lineSegment), lineSegment);
-            lineSegment.a = newLineSegment.a;
-            lineSegment.b = newLineSegment.b;
-            min = (lineSegment.a + lineSegment.b) / 2;
-            System.out.println(lineSegment);
-            raznica = Math.abs(lineSegment.a - lineSegment.b);
-            System.out.println(raznica);
-        }
-        System.out.println(min);
+        LineSegment lineSegment = new LineSegment(-5.0, 9.0);
+        System.out.println(logika.findMin(lineSegment, 1.0));
+    }
 
+    public double function(double x) {
+        double y = x * x;
+        return y;
     }
 
     public Point findFirstPoint(LineSegment lineSegment) {
         Point point = new Point(0.0, 0.0);
         point.x = lineSegment.b - ((lineSegment.b - lineSegment.a) / 1.618);
-        point.y = point.x * point.x;
+        point.y = function(point.x);
         return point;
     }
 
     public Point findSecondPoint(LineSegment lineSegment) {
         Point point = new Point(0.0, 0.0);
         point.x = lineSegment.a + ((lineSegment.b - lineSegment.a) / 1.618);
-        point.y = point.x * point.x;
+        point.y = function(point.x);
         return point;
     }
 
@@ -41,22 +34,25 @@ public class Logika {
         if (x1y1.y >= x2y2.y) {
             newlineSegment.a = x1y1.x;
             newlineSegment.b = lineSegment.b;
-        } else newlineSegment.a = lineSegment.a;
-        newlineSegment.b = x2y2.x;
+        } else {
+            newlineSegment.a = lineSegment.a;
+            newlineSegment.b = x2y2.x;
+        }
         return newlineSegment;
     }
 
-    public double min(LineSegment lineSegment) {
-        LineSegment lineSegment1 = new LineSegment(0.0, 0.0);
-        double abs = Math.abs(lineSegment.b - lineSegment.a);
-        double min = 0.0;
-        while (abs < 0.00001) {
-            LineSegment newLineSegment = createNewLineSegment(findFirstPoint(lineSegment), findSecondPoint(lineSegment), lineSegment1);
+    public double findMin(LineSegment lineSegment, double eps) {
+        Logika logika = new Logika();
+        double min = 0;
+        LineSegment lineSegment1 = new LineSegment(lineSegment.a, lineSegment.b);
+        while (Math.abs(lineSegment1.b - lineSegment1.a) > eps) {
+            LineSegment newLineSegment = logika.createNewLineSegment(findFirstPoint(lineSegment1), findSecondPoint(lineSegment1), lineSegment1);
             lineSegment1.a = newLineSegment.a;
             lineSegment1.b = newLineSegment.b;
-            min = (lineSegment1.a + lineSegment1.b) / 2;
+            min = (newLineSegment.a + newLineSegment.b) / 2;
         }
         return min;
+
     }
 }
 
